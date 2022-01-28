20c71c4bc4d0c79e800e408f749583675618d886
SUDHARSAN44 <88586834+SUDHARSAN44@users.noreply.github.com>
2022-01-28 11:18:07 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,14 @@
+var a;
+
+a="Hi Hello I am Sudharsan";
+
+function displayDate()
+{
+    document.getElementById("demo").innerHTML=Date();
+    alert(document.querySelector('input').value);
+}
+
+for(var i=0;i<5;i++)
+{
+    displayDate()
+}
